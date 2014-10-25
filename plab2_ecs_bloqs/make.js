553844d7cdbd9d234ee10699aa23dd162f2ded0f
Christian Parsons <chparsons@gmail.com>
2014-10-25 10:56:45 -0300

    make.js transforma system factory

--- plab2_ecs_bloqs/make.js
+++ plab2_ecs_bloqs/make.js
@@ -4,12 +4,14 @@ var fs = require('fs');
 var prog = require('commander');
 var setcase = require('change-case');
 
+// TODO agregar systems on the fly en ofApp
+
 // ./make.js mi_xxx_cs
 
 prog
-    .option('-c, --component [name]', 'Add the specified component')
-    .option('-s, --system', 'Add system for the specified component')
-    .option('-r, --remove [name]', 'Remove the specified component')
+    .option('-c, --component [name_with_snake_case]', 'Add the specified component')
+    .option('-s, --system', 'Flag to also add system for the specified component with -c')
+    .option('-r, --remove [name_with_snake_case]', 'Remove the specified component')
     .parse(process.argv);
 
 var comp_name = prog.remove || prog.component;
@@ -17,26 +19,34 @@ var comp_name = prog.remove || prog.component;
 var comp_pascalcase = setcase.pascalCase(comp_name);
 var comp_snakecase = setcase.snakeCase(comp_name);
 
-console.log(' - %s component', comp_name);
-console.log(' - %s component pascal case', comp_pascalcase);
-console.log(' - %s component snake case', comp_snakecase);
+console.log(' - make component name:', comp_name);
+console.log(' - pascal case:', comp_pascalcase);
+console.log(' - snake case:', comp_snakecase);
 
 // files
 
 var comp_filename = comp_pascalcase+'Component.h';
+
+console.log(' - file name:', comp_filename);
+
 var comp_path = __dirname + '/src/components/'+ comp_filename;
 var components_file_path = __dirname + '/src/components/Components.h';
 var comp_include = '#include "components/'+ comp_pascalcase +'Component.h"';
+
+var comp_factory_path = __dirname + '/src/components/ComponentFactory.h'; 
+var comp_factory_code = 'else if (id == "'+ comp_snakecase +'") return new '+ comp_pascalcase +'Component(id);';
+
 
 var sys_filename = comp_pascalcase+'System.h';
 var sys_path = __dirname + '/src/systems/'+ sys_filename;
 var systems_file_path = __dirname + '/src/systems/Systems.h';
 var sys_include = '#include "systems/'+ comp_pascalcase +'System.h"';
 
-var comp_factory_path = __dirname + '/src/components/ComponentFactory.h'; 
-var comp_factory_code = 'else if (id == "'+ comp_snakecase +'") return new '+ comp_pascalcase +'Component(id);';
+var sys_factory_path = __dirname + '/src/add_systems.h'; 
+var sys_factory_code = 'ecs.add_system(new '+ comp_pascalcase +'System());'
 
-// Remove component
+
+//---Remove Component
 
 if ( prog.remove !== undefined ) {
 
@@ -94,7 +104,7 @@ if ( prog.remove !== undefined ) {
         });
     }
 
-    //remove from factory
+    //remove component from factory
     fs.readFile( comp_factory_path, function(err, data) {
         if (err) throw err;
         if (data.toString().indexOf( comp_factory_code ) == -1 ) {
@@ -109,10 +119,25 @@ if ( prog.remove !== undefined ) {
         });
     });
 
+    //remove system from factory
+    fs.readFile( sys_factory_path, function(err, data) {
+        if (err) throw err;
+        if (data.toString().indexOf( sys_factory_code ) == -1 ) {
+            console.log('no factory found on',sys_factory_path);
+            return;
+        }
+        var newdata = data.toString().replace(sys_factory_code + '\n\t','');
+
+        fs.writeFile( sys_factory_path, newdata, function(err) { 
+            if (err) throw err;
+            console.log('removed line for system factory in',sys_factory_path);
+        });
+    });
+
     return;
 }
 
-// Add component
+//---Add Component
 
 // copiar ./src/components/TemplateComponent.h a ./src/components/MiXxxCsComponent.h
 // en MiXxxCsComponent.h reemplazar:
@@ -121,12 +146,12 @@ if ( prog.remove !== undefined ) {
 fs.readFile( __dirname + '/src/components/TemplateComponent.h', function (err, data) {
     if (err) throw err;
 
-    var comp_filecontent = data.toString().replace( /Template/g, comp_pascalcase);
-
     if (fs.existsSync(comp_path)) {
         console.log('warning:',comp_path,'already exists');
         return;
     }
+
+    var comp_filecontent = data.toString().replace( /Template/g, comp_pascalcase);
 
     fs.writeFile( comp_path, comp_filecontent, function (err) {
         if (err) throw err;
@@ -172,8 +197,10 @@ fs.readFile( comp_factory_path, function (err, data) {
 
 });
 
+
 if ( !prog.system ) return;
 
+//---Add System
 
 // copiar ./src/systems/TemplateSystem.h a ./src/systems/MiXxxCsSystem.h
 // en MiXxxCsSystem.h reemplazar:
@@ -184,12 +211,12 @@ if ( !prog.system ) return;
 fs.readFile( __dirname + '/src/systems/TemplateSystem.h', function (err, data) {
     if (err) throw err; 
 
-    var sys_filecontent = data.toString().replace( /Template/g, comp_pascalcase).replace(/template/g, comp_snakecase);
-
     if (fs.existsSync(sys_path)) {
         console.log('warning:',sys_path,'already exists');
         return;
     }
+
+    var sys_filecontent = data.toString().replace( /Template/g, comp_pascalcase).replace(/template/g, comp_snakecase);
 
     fs.writeFile( sys_path, sys_filecontent, function (err) {
         if (err) throw err;
@@ -211,6 +238,26 @@ fs.readFile( systems_file_path, function (err, data) {
     fs.appendFile( systems_file_path, sys_include, function (err) {
         if (err) throw err;
         console.log('system',comp_name,'included in',systems_file_path);
+    });
+
+});
+
+// agregar sistema a add_systems.h
+
+fs.readFile( sys_factory_path, function (err, data) {
+    if (err) throw err;
+
+    if ( data.toString().indexOf( sys_factory_code ) > -1 ) {
+        console.log('warning: system factory code for',comp_name,'already included in',sys_factory_path);
+        return;
+    }
+
+    var placeholder = '//dont remove this line';
+    var newdata = data.toString().replace(placeholder, sys_factory_code + '\n\t' + placeholder);
+
+    fs.writeFile( sys_factory_path, newdata, function(err) { 
+        if (err) throw err;
+        console.log('factory code for system',comp_name,'added in',sys_factory_path);
     });
 
 });
---
+++ plab2_ecs_bloqs/src/add_systems.h
@@ -0,0 +1,16 @@
+/*
+ * this file is tranformed by make.js
+ * TODO only add the systems needed parsing config.json on the fly
+ */
+#include "ofApp.h"
+void ofApp::add_systems()
+{
+  ecs.add_system(new RGBDSystem());
+  ecs.add_system(new ArucoSystem());
+  ecs.add_system(new ParticleSystem());
+  ecs.add_system(new ParticleEmitterSystem());
+  ecs.add_system(new ParticleDepthSlopeConstraintSystem());
+  ecs.add_system(new ParticleFlowFieldSystem());
+  ecs.add_system(new DepthFlowFieldSystem());
+  //dont remove this line
+};
